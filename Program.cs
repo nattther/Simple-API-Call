801ba3ad3aa0bbedf84ac26e0f10da8e1d5423a0
Nathan <nathan.tournant@gmail.com>
2024-04-29 14:51:53 +0200

    Ajout d'une requete Http avec des Header pour l'authentification (car sinon erreur 401) , fonctionnel pour mot predefini.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+private async static Task Main(string[] args)
+{
+    HttpClient client = new HttpClient();
+    client.BaseAddress = new Uri("https://wordsapiv1.p.rapidapi.com/");
+
+
+    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "3aa1e89fdbmsh3c8713a8d3b0981p128c54jsna2fc9089afad");
+    client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "wordsapiv1.p.rapidapi.com");
+
+
+    var response = await client.GetAsync("words/lasagne");
+
+
+    Console.WriteLine($"Status Code: {response.StatusCode} Content: {await response.Content.ReadAsStringAsync()}");
+    Console.ReadLine();
 }
+
+    }
