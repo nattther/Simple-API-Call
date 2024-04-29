94d896591ff92e8429ef43193cb96245957b53c9
Nathan <nathan.tournant@gmail.com>
2024-04-29 17:04:39 +0200

    Clean code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Text.Json;
+﻿using System.Net.Http.Json;
 using System.Text.Json.Serialization;
-using System.Net.Http.Json;
-using System.Reflection.Metadata;
 
 
 internal class Program
@@ -16,78 +11,64 @@ internal class Program
         foreach (var mot in mots)
         {
             ThreadPool.QueueUserWorkItem(async state =>
-{
-    await MakeresquestWord(mot);
-});
+            {
+                await MakeRequestForWord(mot);
+            });
         }
 
         ThreadPool.QueueUserWorkItem(async state =>
         {
-            await MakeresquestRiot();
+            await MakeRequestForRiot();
         });
 
         await Task.Delay(5000);
 
-        Console.WriteLine("Les requetes ont bien été faites.");
+        Console.WriteLine("Les requêtes ont bien été faites.");
     }
-    private static async Task MakeresquestWord(string mot)
+
+    private static async Task MakeRequestForWord(string word)
     {
-        Console.WriteLine($"Début de requête {mot}");
+        Console.WriteLine($"Début de la requête pour {word}");
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri("https://wordsapiv1.p.rapidapi.com/");
-
         client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "3aa1e89fdbmsh3c8713a8d3b0981p128c54jsna2fc9089afad");
         client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "wordsapiv1.p.rapidapi.com");
 
-        var response = await client.GetAsync($"words/{mot}");
-
-
+        var response = await client.GetAsync($"words/{word}");
         string jsonContent = await response.Content.ReadAsStringAsync();
-
-        string filePath = $"{mot}.json";
-
-
+        string filePath = $"{word}.json";
         await File.WriteAllTextAsync(filePath, jsonContent);
 
-        Console.WriteLine($"Le contenu a été enregistré avec succès dans {filePath}");
-
-
-        WordApiResponse resultat = await response.Content.ReadFromJsonAsync<WordApiResponse>();
+        WordApiResponse result = await response.Content.ReadFromJsonAsync<WordApiResponse>();
         Console.WriteLine("Résultat");
-        foreach (Result result in resultat.Results)
+        foreach (Result item in result.Results)
         {
-            Console.WriteLine($"Définition : {result.Definition}");
+            Console.WriteLine($"Définition : {item.Definition}");
         }
 
-
-
-        Console.WriteLine($"Fin de requête {mot}");
+        Console.WriteLine($"Fin de la requête pour {word}");
     }
-    private static async Task MakeresquestRiot()
+
+    private static async Task MakeRequestForRiot()
     {
+        Console.WriteLine("Début de la requête pour Riot ");
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://euw1.api.riotgames.com/lol/platform/v3/");
+
+        var response = await client.GetAsync("champion-rotations?api_key=RGAPI-a34c9265-d674-403a-b58c-c115810e1df6");
+
+        Console.WriteLine($"Code de statut : {response.StatusCode}");
+
+        ChampionRotation rotation = await response.Content.ReadFromJsonAsync<ChampionRotation>();
+
+        Console.WriteLine("Champions gratuits:");
+        foreach (int id in rotation.FreeChampionIds)
         {
-            Console.WriteLine("Début de requete Riot ");
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://euw1.api.riotgames.com/lol/platform/v3/");
-
-            var response = await client.GetAsync("champion-rotations?api_key=RGAPI-a34c9265-d674-403a-b58c-c115810e1df6");
-
-            Console.WriteLine($"Status Code: {response.StatusCode}");
-
-
-            ChampionRotation rotation = await response.Content.ReadFromJsonAsync<ChampionRotation>();
-
-            Console.WriteLine("Free Champion IDs:");
-            foreach (int id in rotation.FreeChampionIds)
-            {
-                Console.Write(id + " ");
-            }
-
-            Console.WriteLine("Fin de requete Riot");
+            Console.Write(id + " ");
         }
+
+        Console.WriteLine("Fin de la requête pour Riot");
     }
-
-
 
     public class ChampionRotation
     {
@@ -100,6 +81,7 @@ internal class Program
         [JsonPropertyName("maxNewPlayerLevel")]
         public int MaxNewPlayerLevel { get; set; }
     }
+
     public class WordApiResponse
     {
         [JsonPropertyName("word")]
@@ -145,4 +127,3 @@ internal class Program
         public string All { get; set; }
     }
 }
-
